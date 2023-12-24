ec78d9b7615fc5bb97db44390e9031ac634bd1b0
Amit Pawade <amit.pawade@gordian.com>
2023-12-24 12:57:02 +0530

    commit msg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component:HomeComponent},
+
+    {path: 'st', loadChildren: () => import('./students/student.routes').then((mod)=> mod.StudentRoutes)}
+];
+
---
+++ src/app/students/student.routes.ts
@@ -0,0 +1,16 @@
+import { StudentListComponent } from "./student-list/student-list.component";
+import { StudentComponent } from "./student/student.component";
+
+
+
+export const StudentRoutes = [
+    // {path : '', 
+    // children : [ 
+    // {path: 'student-list', component: StudentListComponent},
+    // {path : 'student',component: StudentComponent}
+
+    {path: 'student-list', component: StudentListComponent},
+    {path : 'student',component: StudentComponent}
+]
+
+   
